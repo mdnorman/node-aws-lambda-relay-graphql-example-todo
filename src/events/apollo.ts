a966cebffc7fb44983c2c1395210d40bccf794fd
Michael D. Norman <michael.norman@denormans.com>
2018-09-10 22:41:39 -0500

    added example settings

--- src/events/apollo.ts
+++ src/events/apollo.ts
@@ -2,11 +2,14 @@ import { ApolloServer, gql } from 'apollo-server-lambda';
 
 const typeDefs = gql`
   type Query {
+    hello: String
   }
 `;
 
 const resolvers = {
-  Query: {},
+  Query: {
+    hello: () => 'Hello world!',
+  },
 };
 
 const server = new ApolloServer({
